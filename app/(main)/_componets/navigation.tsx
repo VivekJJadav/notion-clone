b478f9ee80b8c587f3061eb6f97956726718c610
Vivek jadav <150367108+VivekJJadav@users.noreply.github.com>
2024-06-04 21:11:05 +0530

    Update navigation.tsx

--- app/(main)/_componets/navigation.tsx
+++ app/(main)/_componets/navigation.tsx
@@ -131,71 +131,76 @@ export const Navigation = () => {
     });
   };
 
-  return (
-  <>
-    <aside
-      ref={sidebarRef}
-      className={cn(
-        "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
-        isResetting && "transition-all ease-in-out duration-300",
-        isMobile && "w-0"
-      )}
-    >
-      <div
-        onClick={collapse}
-        role="button"
-        className={cn(
-          "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
-          isMobile && "opacity-100"
-        )}
-      >
-        <ChevronLeft className="h-6 w-6" />
-      </div>
-      <div>
-        <UserItem />
-        <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-        <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
-        <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
-      </div>
-      <div className="mt-4">
-        <DocumentList />
-        <Item onClick={handleCreate} icon={Plus} label="Add a page" />
-        <Popover>
-          <PopoverTrigger className="w-full mt-4">
-            <Item label="Trash" icon={Trash} />
-          </PopoverTrigger>
-          <PopoverContent
-            side={isMobile ? "bottom" : "right"}
-            className="p-0 w-72"
-          >
-            <TrashBox />
-          </PopoverContent>
-        </Popover>
-      </div>
-      <div
-        onMouseDown={handleMouseDown}
-        onClick={resetWidth}
-        className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
-      />
-    </aside>
-    <div
-      ref={navbarRef}
-      className={cn(
-        "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
-        isResetting && "transition-all ease-in-out duration-300",
-        isMobile && "left-0 w-full"
-      )}
-    >
-      <nav className="bg-transparent px-3 py-2 w-full relative">
-        {isCollapsed && (
-          <MenuIcon
-            onClick={resetWidth}
+    return (
+      <>
+        <aside
+          ref={sidebarRef}
+          className={cn(
+            "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+            isResetting && "transition-all ease-in-out duration-300",
+            isMobile && "w-0"
+          )}
+        >
+          <div
+            onClick={collapse}
             role="button"
-            className="h-6 w-6 text-muted-foreground absolute left-2 top-2"
+            className={cn(
+              "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+              isMobile && "opacity-100"
+            )}
+          >
+            <ChevronLeft className="h-6 w-6" />
+          </div>
+          <div>
+            <UserItem />
+            <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
+            <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
+            <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
+          </div>
+          <div className="mt-4">
+            <DocumentList />
+            <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+            <Popover>
+              <PopoverTrigger className="w-full mt-4">
+                <Item label="Trash" icon={Trash} />
+              </PopoverTrigger>
+              <PopoverContent
+                side={isMobile ? "bottom" : "right"}
+                className="p-0 w-72"
+              >
+                <TrashBox />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div
+            onMouseDown={handleMouseDown}
+            onClick={resetWidth}
+            className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
           />
-        )}
-      </nav>
-    </div>
-  </>
-);
-};
+        </aside>
+        <div
+          ref={navbarRef}
+          className={cn(
+            "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+            isResetting && "transition-all ease-in-out duration-300",
+            isMobile && "left-0 w-full"
+          )}
+        >
+          {!params.DocumentId ? (
+            <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+          ) : (
+            // <p></p>
+            <nav className="bg-transparent px-3 py-2 w-full relative">
+              {isCollapsed && (
+                <MenuIcon
+                  onClick={resetWidth}
+                  role="button"
+                  className="h-6 w-6 text-muted-foreground absolute left-2 top-2"
+                />
+              )}
+            </nav>
+          )}
+        </div>
+      </>
+    );
+  };

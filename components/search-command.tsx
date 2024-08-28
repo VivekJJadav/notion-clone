1e43ad3c06adbfc8f01257f11d8b91202718afd9
Vivek jadav <150367108+VivekJJadav@users.noreply.github.com>
2024-08-28 22:35:29 +0530

    Update search-command.tsx

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -62,7 +62,7 @@ export const SearchCommand = () => {
             <CommandItem
               key={document._id}
               value={`${document._id}-${document.title}`}
-              onSelect={onSelect}
+              onSelect={() => onSelect(document._id)}
             >
               {document.icon ? (
                 <p className="mr-2 text-[18px]">{document.icon}</p>
